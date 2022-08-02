138fb2d89520fb510f52b23cd67298305a83d62b
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-08-02 15:10:56 -0400

    feat: html and styles for nav and menu

---
+++ app.js
@@ -0,0 +1,27 @@
+// Selectors
+const menuToggleButton = document.getElementById("toggleMenu");
+const menu = document.getElementById("menu");
+const menuList = document.getElementById("menu-list");
+const menuListItems = document.querySelectorAll(".menu-item");
+
+// Theme State
+let isDarkMode = false;
+
+// Functions
+menuToggleButton.addEventListener("click", () => {
+  menu.classList.toggle("menu-expanded");
+  menuToggleButton.classList.toggle("open");
+  if (menu.classList.contains("menu-expanded")) {
+    menuList.classList.remove("menu-list__hidden");
+    menuList.classList.add("menu-list__expanded");
+    menuListItems.forEach((item) => {
+      item.classList.add("menu-list-item__visible");
+    });
+  } else {
+    menuList.classList.remove("menu-list__expanded");
+    menuList.classList.add("menu-list__hidden");
+    menuListItems.forEach((item) => {
+      item.classList.remove("menu-list-item__visible");
+    });
+  }
+});
