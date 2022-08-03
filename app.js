4956a90a69e9619ef5925f95502e6daceb4eb4dd
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-08-03 09:40:41 -0400

    refactor: clean up js file to make more readable

--- app.js
+++ app.js
@@ -9,41 +9,7 @@ const heroName = document.querySelector(".hero-name");
 let isMenuOpen = false;
 let isDarkMode = false;
 
-// Functions
-menuToggleButton.addEventListener("click", () => {
-  menu.classList.toggle("menu-expanded");
-  menuToggleButton.classList.toggle("open");
-  if (menu.classList.contains("menu-expanded")) {
-    menuList.classList.remove("menu-list__hidden");
-    menuList.classList.add("menu-list__expanded");
-    menuListItems.forEach((item) => {
-      item.classList.add("menu-list-item__visible");
-    });
-    isMenuOpen = true;
-  } else {
-    menuList.classList.remove("menu-list__expanded");
-    menuList.classList.add("menu-list__hidden");
-    menuListItems.forEach((item) => {
-      item.classList.remove("menu-list-item__visible");
-    });
-    isMenuOpen = false;
-  }
-});
-
-window.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && isMenuOpen) {
-    menu.classList.remove("menu-expanded");
-    menuToggleButton.classList.remove("open");
-    console.log("hello world");
-    menuList.classList.remove("menu-list__expanded");
-    menuList.classList.add("menu-list__hidden");
-    menuListItems.forEach((item) => {
-      item.classList.remove("menu-list-item__visible");
-    });
-    isMenuOpen = false;
-  }
-});
-
+// Typewriter
 const typewriter = new Typewriter(heroName, {
   loop: false,
   delay: "natural",
@@ -55,3 +21,37 @@ typewriter
   .deleteChars(5)
   .typeString("arino")
   .start();
+
+// Functions
+const openMenu = () => {
+  menuList.classList.remove("menu-list__hidden");
+  menuList.classList.add("menu-list__expanded");
+  menuListItems.forEach((item) => {
+    item.classList.add("menu-list-item__visible");
+  });
+  isMenuOpen = true;
+};
+
+const closeMenu = () => {
+  menuList.classList.remove("menu-list__expanded");
+  menuList.classList.add("menu-list__hidden");
+  menuListItems.forEach((item) => {
+    item.classList.remove("menu-list-item__visible");
+  });
+  isMenuOpen = false;
+};
+
+// EventListeners
+menuToggleButton.addEventListener("click", () => {
+  menu.classList.toggle("menu-expanded");
+  menuToggleButton.classList.toggle("open");
+  menu.classList.contains("menu-expanded") ? openMenu() : closeMenu();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && isMenuOpen) {
+    menu.classList.remove("menu-expanded");
+    menuToggleButton.classList.remove("open");
+    closeMenu();
+  }
+});
